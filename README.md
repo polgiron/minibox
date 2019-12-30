aeb8ed5d8bd58f3b0108c80f4e5c146beef389cb
Jozo <jozo.robert@gmail.com>
2019-12-30 12:55:01 +0100

    Add venv and setup file

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # minibox
 Jukebox in a minitel
+
+## Run in a virtual env
+Use python 3 for the next commands. ``python --version``
+1. Install virtualenv ``pip install virtualenv``
+2. Create the venv ``virtualenv venv``
+3. Start it ``source venv/bin/activate``
+4. Install the dependencies ``python setup.py install``
+
+Now you can run the application inside the virtual env.
+To stop it : ``deactivate``
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='minibox',
+    version='0.1',
+    packages=find_packages(exclude=["tests"]),
+    url='',
+    license='',
+    author='Pol Giron',
+    description='',
+    include_package_data=True,
+    package_data={
+        '': ['*.avsc'],
+    },
+    install_requires=[
+        'urwid'
+    ],
+    entry_points={
+        'console_scripts': [
+            ' minibox = minibox:main',
+        ],
+    },
+)
